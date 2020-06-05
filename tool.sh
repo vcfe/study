c8eda6c83f040af84949b8e339d4ee8158f1ce08
vcfe <anenui@qq.com>
2020-06-05 17:04:49 +0800

    First Commit

--- tool.sh
+++ tool.sh
@@ -1,1 +1,6 @@
-#大家好
+#!/bin/bash
+sudo -i
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^PermitRootLogin/PermitRootLogin yes/' /etc/ssh/sshd_config
+passwd root
+systemctl restart sshd
