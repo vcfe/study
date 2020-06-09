f18a0f0b864054e846f9fd8339ac3a7bb1955362
vcfe <anenui@qq.com>
2020-06-09 18:15:39 +0800

    First Commit

--- tool.sh
+++ tool.sh
@@ -3,4 +3,6 @@ sudo -i
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^PermitRootLogin/PermitRootLogin yes/' /etc/ssh/sshd_config
 passwd root
-systemctl restart sshd
+systemctl restart ssh
+d=$(cd `dirname $0`; pwd)
+echo $d$0
